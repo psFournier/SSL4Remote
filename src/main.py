6569813f135bd29bdbce42e88dadc3c92c771179
Pierre Fournier <psfournier@mailbox.org>
2021-03-10 18:40:10 +0100

    Cleaning code for shareable template

--- src/main.py
+++ src/main.py
@@ -1,41 +1,28 @@
-"""
-In this file is a very simple code training a Unet on ISPRS_VAIHINGEN.
-The Unet should come from segmentation_models.pytorch.
-We do not use Pytorch.Lightning nor Hydra, but this files intends to make
-clear what these two layers will replace later.
-Being some kind of tutorial file, it should be self-contained.
-The file follows the sampleISPRS from ai4geo_dl
-"""
-
-import os
 import datetime
-
-from pytorch_lightning import Trainer, loggers
-from pl_modules import Semisup_segm
-from pl_datamodules import Isprs_semisup
-from networks import Unet
-# from metrics import MAPMetric
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
-import pytorch_lightning.metrics as M
-from metrics import MyMetricCollection
-import segmentation_models_pytorch as smp
-from callbacks import Conf_mat_logger, Array_val_logger
+import os
 from argparse import ArgumentParser
-import shutil
-from pytorch_lightning.profiler import PyTorchProfiler, SimpleProfiler
-import torch.autograd.profiler as profiler
 from copy import deepcopy
+
+import albumentations as A
+import pytorch_lightning.metrics as M
+import segmentation_models_pytorch as smp
+from albumentations.pytorch import ToTensorV2
+from pytorch_lightning import Trainer, loggers
+
+from callbacks import Array_val_logger, Conf_mat_logger
+from metrics import MyMetricCollection
+from networks import Unet
+from pl_datamodules import Isprs_semisup
+from pl_modules import Semisup_segm
+
 
 def main():
 
-    parser = ArgumentParser(add_help=True)
+    parser = ArgumentParser()
+    parser.add_argument("--output_dir", type=str, default="./outputs")
 
-    parser.add_argument('--output_dir',
-                        type=str,
-                        default='~/scratch',
-                        help='Where to store results')
-
+    # Each class of interest contains a method to add its specific arguments
+    # to the parser
     parser = Unet.add_model_specific_args(parser)
     parser = Isprs_semisup.add_model_specific_args(parser)
     parser = Semisup_segm.add_model_specific_args(parser)
@@ -45,29 +32,23 @@ def main():
 
     current_date = datetime.datetime.now().strftime("%Y-%m-%d")
     log_dir = os.path.expanduser(
-        os.path.join(
-            args.output_dir, '%s_%s' % (current_date, 'unet_isprs/')
-        )
+        os.path.join(args.output_dir, "%s_%s" % (current_date, "unet_isprs/"))
     )
-    TB_logger = loggers.TensorBoardLogger(save_dir=log_dir,
-                                          name='tensorboard')
-    CSV_logger = loggers.CSVLogger(save_dir=log_dir,
-                                   name='csv')
+    tensorboard = loggers.TensorBoardLogger(save_dir=log_dir, name="tensorboard")
 
-    # Create network
-    network = Unet(args.in_channels, args.num_classes)
-    # network = smp.Unet(
-    #     encoder_name='efficientnet-b0',
-    #     encoder_weights='imagenet',
-    #     in_channels=args.in_channels,
-    #     classes=args.num_classes,
-    #     decoder_attention_type='scse'
-    # )
+    # The effect of using imagenet pre-training instead is to be measured, but
+    # for now we don't.
+    network = smp.Unet(
+        encoder_name="efficientnet-b0",
+        encoder_weights=None,
+        in_channels=args.in_channels,
+        classes=args.num_classes,
+    )
 
-    transform = A.Compose([
-        ToTensorV2()
-    ])
+    # Additional transforms should be employed: which ones?
+    transform = A.Compose([ToTensorV2()])
 
+    # The lightning datamodule deals with instantiating the proper dataloaders.
     pl_datamodule = Isprs_semisup(
         args.data_dir,
         args.crop_size,
@@ -75,102 +56,62 @@ def main():
         args.batch_size,
         sup_train_transforms=transform,
         val_transforms=transform,
-        unsup_train_transforms=transform
+        unsup_train_transforms=transform,
     )
 
-    accuracy = M.Accuracy(
-        top_k=1,
-        subset_accuracy=False
-    )
-
+    # Scalar metrics are separated because lightning can deal with logging
+    # them automatically.
+    accuracy = M.Accuracy(top_k=1, subset_accuracy=False)
     global_precision = M.Precision(
-        num_classes=args.num_classes,
-        mdmc_average='global',
-        average='macro'
+        num_classes=args.num_classes, mdmc_average="global", average="macro"
     )
-
-    IoU = M.IoU(
-        num_classes=args.num_classes,
-        reduction='elementwise_mean'
-    )
+    iou = M.IoU(num_classes=args.num_classes, reduction="elementwise_mean")
 
     scalar_metrics_dict = {
-        'acc': accuracy,
-        'global_precision': global_precision,
-        'IoU': IoU
+        "acc": accuracy,
+        "global_precision": global_precision,
+        "IoU": iou,
     }
-    train_scalar_metrics = MyMetricCollection(
-        scalar_metrics_dict,
-        "train_"
-    )
-    val_scalar_metrics = MyMetricCollection(
-        deepcopy(scalar_metrics_dict),
-        "val_"
-    )
 
+    # Two things here:
+    # 1. MyMetricCollection adds a prefix to metrics names, and should be
+    # included in future versions of lightning
+    # 2. The metric objects keep statistics during runs, and deepcopy should be
+    # necessary to ensure these stats do not overlap
+    train_scalar_metrics = MyMetricCollection(scalar_metrics_dict, "train_")
+    val_scalar_metrics = MyMetricCollection(deepcopy(scalar_metrics_dict), "val_")
 
+    # This lightning module is where the training schema is implemented.
     pl_module = Semisup_segm(
-        network = network,
-        scalar_metrics={
-            "train": train_scalar_metrics,
-            "val": val_scalar_metrics
-        },
-        unsup_loss_prop=args.unsup_loss_prop
+        network=network,
+        scalar_metrics={"train": train_scalar_metrics, "val": val_scalar_metrics},
+        unsup_loss_prop=args.unsup_loss_prop,
     )
 
+    # Non-scalar metrics are bundled in callbacks that deal with logging them
     per_class_precision = M.Precision(
-        num_classes=args.num_classes,
-        mdmc_average='global',
-        average='none'
+        num_classes=args.num_classes, mdmc_average="global", average="none"
     )
     per_class_precision_logger = Array_val_logger(
-        array_metric=per_class_precision,
-        name='per_class_precision'
+        array_metric=per_class_precision, name="per_class_precision"
     )
-
-    per_class_F1 = M.F1(
-        num_classes=args.num_classes,
-        average='none'
-    )
+    per_class_F1 = M.F1(num_classes=args.num_classes, average="none")
     per_class_F1_logger = Array_val_logger(
-        array_metric=per_class_F1,
-        name='per_class_F1'
+        array_metric=per_class_F1, name="per_class_F1"
     )
-    cm = Conf_mat_logger(
-        num_classes=args.num_classes
-    )
+    cm = Conf_mat_logger(num_classes=args.num_classes)
 
-    # profiler = PyTorchProfiler(
-    #     output_filename=os.path.join(log_dir, 'profile'),
-    #     # profile_memory=False,
-    #     # use_cpu=True,
-    #     # use_cuda=False
-    # )
-
+    # Using from_argparse_args enables to use any standard parameter of the
+    # lightning Trainer class without having to manually add them to the parser.
     trainer = Trainer.from_argparse_args(
         args,
-        logger=TB_logger,
-        callbacks=[
-            cm,
-            per_class_precision_logger,
-            per_class_F1_logger
-        ],
-        # profiler=profiler
+        logger=tensorboard,
+        callbacks=[cm, per_class_precision_logger, per_class_F1_logger],
     )
 
-    # with profiler.profile(with_stack=True, profile_memory=True) as prof:
-    #     trainer.fit(
-    #         model=pl_module,
-    #         datamodule=pl_datamodule
-    #     )
-    # print(prof.key_averages(group_by_stack_n=5).table(
-    #     sort_by='self_cpu_time_total', row_limit=5))
+    trainer.fit(model=pl_module, datamodule=pl_datamodule)
 
-    trainer.fit(
-        model=pl_module,
-        datamodule=pl_datamodule
-    )
 
 if __name__ == "__main__":
-    # execute only if run as a script
+
     main()
