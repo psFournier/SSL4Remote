fdac64b6976168a50a158a66e50735220468fabb
Pierre Fournier <psfournier@mailbox.org>
2021-04-20 14:55:27 +0200

    arguments for read from tb event script

--- src/read_from_tb_event.py
+++ src/read_from_tb_event.py
@@ -1,11 +1,23 @@
+from argparse import ArgumentParser
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+import os
+
+parser = ArgumentParser()
+parser.add_argument("--path", type=str)
+parser.add_argument("--scalar", type=str)
+
+args = parser.parse_args()
+
 event_acc = EventAccumulator(
-    '/home/pierre/PycharmProjects/RemoteSensing/outputs'
-    '/baseline_christchurch_noaug_2021-04-16/events.out.tfevents.1618562118.bender.2376589.0')
+    os.path.join('/home/pierre/PycharmProjects/RemoteSensing/outputs', args.path)
+)
+
+
 event_acc.Reload()
 # Show all tags in the log file
 
 # E. g. get wall clock, number of steps and value for a scalar 'Accuracy'
 # w_times, step_nums, vals = zip(*event_acc.Scalars('val_IoU'))
-for w_time, step_num, val in event_acc.Scalars('val_IoU')[::20]:
+for w_time, step_num, val in event_acc.Scalars(args.scalar):
     print(step_num, val)
+
