e3f999da1f2d342f76128e584ff4601c50d92e2b
Pierre Fournier <psfournier@mailbox.org>
2021-04-28 16:12:28 +0200

    working on phr images

--- src/extract_window_from_tif.py
+++ src/extract_window_from_tif.py
@@ -3,12 +3,11 @@ from rasterio.windows import Window
 
 raster = rasterio.open("/work/OT/ai4geo/DATA/REF/COS_4/PARIS/20180326/IMG_PHR1A_PMS_201803261058346_SEN_3625348101-006_R1C1_PROJECTED_g.tif")
 out_profile = raster.profile
-w = raster.read(window=Window(0, 0, 500, 500))
-number_of_bands, height, width = w.shape
+w = raster.read(window=Window(0, 0, 1000, 1000))
+_, height, width = w.shape
 out_profile.update(
     height=height,
     width=width,
-    count=number_of_bands
 )
-with rasterio.open("/home/eh/fournip/SemiSupervised/SSL4Remote/window.tif", 'w', **out_profile) as dst:
+with rasterio.open("/home/eh/fournip/SemiSupervised/SSL4Remote/paris.tif", 'w', **out_profile) as dst:
     dst.write(w)
