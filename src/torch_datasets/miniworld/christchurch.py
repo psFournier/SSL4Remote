cfd8a130c09889c60fe2511674955d8e6f101ad1
Pierre Fournier <psfournier@mailbox.org>
2021-05-04 19:14:26 +0200

    fixed christchurch labels

--- src/torch_datasets/miniworld/christchurch.py
+++ src/torch_datasets/miniworld/christchurch.py
@@ -7,18 +7,19 @@ import numpy as np
 class Christchurch(Base, ABC):
 
     nb_unlabeled_images = 0
-    image_size = (1500,1500)
+    image_size = (1500, 1500)
     # pixels_per_class = [1481907417, 148316583]
     # mean_labeled_pixels = (0.4050, 0.4140, 0.3783)
     # std_labeled_pixels = (0.2102, 0.2041, 0.1965)
-    default_train_val = (857, 94)
+    # default_train_val = (857, 94)
+    default_train_val = (2, 2)
     nb_labeled_images = default_train_val[0] + default_train_val[1]
 
     @staticmethod
     def colors_to_labels(labels_color):
 
         labels = np.zeros(labels_color.shape[:2], dtype=int)
-        labels[np.where(np.any(labels_color != [0, 0, 0], axis=2))] = 1
+        labels[np.where(np.any(labels_color != [0], axis=2))] = 1
 
         return labels
 
