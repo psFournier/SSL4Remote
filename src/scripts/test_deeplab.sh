e2b9dcd9dd76c0f6689bc9845ff265564c98f672
Pierre Fournier <psfournier@mailbox.org>
2021-06-08 12:27:01 +0200

    test script deeplab

--- src/scripts/test_deeplab.sh
+++ src/scripts/test_deeplab.sh
@@ -5,6 +5,11 @@ PYTHON="$HOME"/venv/bin/python
 SCRIPT="$HOME"/src/test.py
 
 "${PYTHON}" "${SCRIPT}" \
---ckpt_path "$HOME"/outputs/sup_mw_austin_2-2_d4/version_1/checkpoints/epoch=0-step=4.ckpt \
+--ckpt_path "$HOME"/outputs/sup_mw_austin_31-5/version_0/checkpoints/epoch=418-step=131146.ckpt \
 --data_dir /scratch_ai4geo/miniworld_tif \
---gpus 1
+--gpus 1 \
+--image_path /scratch_ai4geo/miniworld_tif/austin/train/1_x.tif \
+--label_path /scratch_ai4geo/miniworld_tif/austin/train/1_y.tif \
+--with_swa \
+--img_aug d4 hue \
+--tta d4
