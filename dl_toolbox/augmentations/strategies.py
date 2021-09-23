96e73748ce28f5419f71d036ee440efa0dd62924
Pierre Fournier <pierre00fournier@gmail.com>
2021-09-23 17:34:05 +0200

    expe without void and with corrected visu

--- dl_toolbox/augmentations/strategies.py
+++ dl_toolbox/augmentations/strategies.py
@@ -1,6 +1,38 @@
 from dl_toolbox.augmentations import *
 
-def hard_transformation():
+def d4_transformation():
+
+    out = OneOf(
+        transforms=[
+            NoOp(),
+            D4(p=1)
+        ],
+        transforms_ps=[1, 1]
+    )
+
+    return out
+
+
+def color_transformation():
+
+    out = OneOf(
+        transforms=[
+            NoOp(),
+            OneOf(
+                transforms=[
+                    Gamma(p=1),
+                    Contrast(p=1),
+                    Saturation(p=1),
+                    Brightness(p=1)
+                ],
+                transforms_ps=[1, 1, 1, 1])
+        ],
+        transforms_ps=[1, 1]
+    )
+
+    return out
+
+def d4_color_transformation():
 
     T = Compose([
         D4(p=1),
@@ -26,6 +58,10 @@ def hard_transformation():
 
 def get_transforms(name: str):
 
-    if name == 'hard':
-        return hard_transformation()
+    if name == 'd4_color':
+        return d4_color_transformation()
+    elif name == 'd4':
+        return d4_transformation()
+    elif name == 'color':
+        return color_transformation()
     return NoOp()
