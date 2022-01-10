9e138dec2fcb9707a274ec2ca366abf3a6c1015c
Pierre <pierre.fournier@onera.fr>
2022-01-10 13:43:39 +0100

    predict running; no validated

--- dl_toolbox/scripts/pred_local.sh
+++ dl_toolbox/scripts/pred_local.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-HOME=/d/pfournie/semi-supervised-learning
-PYTHON="$HOME"/venv/bin/python
-SCRIPT="$HOME"/examples/miniworld/predict.py
+REPO=/home/pfournie/semi-supervised-learning
+PYTHON="$REPO"/venv/bin/python
+SCRIPT="$REPO"/dl_toolbox/predict.py
 
 "${PYTHON}" "${SCRIPT}" \
---ckpt_path "$HOME"/outputs/christchurch/christchurch_aug/version_0/epoch=400-step=125200.ckpt \
---gpus 1 \
---image_path /scratch_ai4geo/miniworld_tif/vienna/train/0_x.tif \
---label_path /scratch_ai4geo/miniworld_tif/vienna/train/0_y.tif \
---output_path /scratch_ai4geo/miniworld_tif/vienna/train/0_x_pred.tif \
---workers 6 \
+--ckpt_path "$REPO"/outputs/test_semcity/version_0/checkpoints/epoch=5-step=5.ckpt \
+--image_path /home/pfournie/ai4geo/data/SemcityTLS_DL/val/TLS_BDSD_M_04.tif \
+--label_path /home/pfournie/ai4geo/data/SemcityTLS_DL/val/TLS_GT_04.tif \
+--output_path "$REPO"/outputs/test_semcity/version_0/pred_04.tif \
+--workers 0 \
 --batch_size 16 \
---tile_size 128 \
---tile_step 128
+--tile_size 128 128 \
+--tile_step 128 128
