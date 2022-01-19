6649d0767d5afb6ddbe190cecaa5bcd708c8897f
Pierre <pierre.fournier@onera.fr>
2022-01-19 14:46:05 +0100

    removed scripts for toolbox

--- dl_toolbox/scripts/train_spiro.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=test    # -J nom-job      => nom du job
-#SBATCH --ntasks=1           # -n 24           => nombre de taches (obligatoire)
-#SBATCH --time 0-2:00         # -t 0-2:00       => duree (JJ-HH:MM) (obligatoire)
-#SBATCH --qos=co_long_gpu       #                 => QOS choisie (obligatoire)
-echo "not implemented"
