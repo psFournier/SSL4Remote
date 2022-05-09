9e668ef2249844bb0afd2de35ec65c78959d813b
Pierre Fournier <pierre00fournier@gmail.com>
2022-05-09 15:50:31 +0200

    add script example

---
+++ docs/examples/unet_semcity.py
@@ -0,0 +1,52 @@
+from argparse import ArgumentParser
+from pytorch_lightning.callbacks import ModelCheckpoint,StochasticWeightAveraging
+from pytorch_lightning import Trainer 
+from pytorch_lightning.profiler import SimpleProfiler
+from pytorch_lightning.loggers import TensorBoardLogger
+from dl_toolbox.lightning_modules import Unet
+from dl_toolbox.lightning_datamodules import SemcityBdsdDm
+from dl_toolbox.callbacks import SegmentationImagesVisualisation, ConfMatLogger 
+
+
+def main():
+
+    parser = ArgumentParser()
+    parser.add_argument("--output_dir", type=str, default="./outputs")
+    parser = Unet.add_model_specific_args(parser)
+    parser = SemcityBdsdDm.add_model_specific_args(parser)
+    parser = Trainer.add_argparse_args(parser)
+    args = parser.parse_args()
+    args_dict = vars(args)
+
+    pl_datamodule = SemcityBdsdDm(**args_dict)
+    pl_module = Unet(**args_dict)
+    trainer = Trainer.from_argparse_args(
+        args,
+        logger=TensorBoardLogger(args.output_dir, name='semcity'),
+        profiler=SimpleProfiler(),
+        callbacks=[
+            ModelCheckpoint(monitor='Val_Dice', mode='min'),
+            ModelCheckpoint(),
+            SegmentationImagesVisualisation(),
+            StochasticWeightAveraging(
+                swa_epoch_start=0.91,
+                swa_lrs=0.005,
+                annealing_epochs=1,
+                annealing_strategy='linear',
+                device=None
+            ),
+            ConfMatLogger()
+        ],
+        log_every_n_steps=300,
+        flush_logs_every_n_steps=1000,
+        num_sanity_val_steps=0,
+        check_val_every_n_epoch=1,
+        benchmark=True
+    )
+
+    trainer.fit(model=pl_module, datamodule=pl_datamodule)
+
+
+if __name__ == "__main__":
+
+    main()
---
+++ docs/examples/unet_semcity.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DATA=/d/pfournie/ai4geo/data
+PYTHON=/d/pfournie/ai4geo/venv/bin/python3
+SCRIPT=/d/pfournie/ai4geo/python_scripts/unet_semcity.py
+
+"${PYTHON}" "${SCRIPT}" \
+--data_path /d/pfournie/ai4geo/data/SemcityTLS_DL \
+--splitfile_path /d/pfournie/ai4geo/split_semcity.csv \
+--test_fold 4 \
+--output_dir /d/pfournie/ai4geo/outputs \
+--sup_batch_size 8 \
+--workers 6 \
+--max_epochs 100 \
+--epoch_len 2000 \
+--crop_size 256 \
+--num_classes 8 \
+--img_aug d4_color-0 \
+--batch_aug no \
+--encoder efficientnet-b5 \
+--in_channels 3 \
+--initial_lr 0.05 \
+--final_lr 0.001 \
+--lr_milestones 0.5 0.9 \
+--weight_decay 0. \
+--pretrained \
+--train_with_void \
+--limit_train_batch 1 \
+--gpus 1
