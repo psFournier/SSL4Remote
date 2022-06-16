35c91b9da8c4c46314901967bcdfc1cc185f9ee3
Pierre Fournier <pierre.fournier@onera.fr>
2022-06-16 12:11:13 +0000

    fixed full_raster_path in csv reading

--- dl_toolbox/lightning_datamodules/utils.py
+++ dl_toolbox/lightning_datamodules/utils.py
@@ -44,7 +44,7 @@ def read_splitfile(
 
         try:
             orig_img = row[9]
-            kwargs['full_raster_path'] = os.path.join(data_path, city, orig_img)
+            kwargs['full_raster_path'] = os.path.join(data_path, orig_img)
         except:
             pass
 
