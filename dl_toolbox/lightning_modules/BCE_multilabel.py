b60c4962eb08ac7652bcf2c6abbaeca9d21ab7ac
Pierre <pierre.fournier@onera.fr>
2022-08-29 11:39:20 +0200

    networks

--- dl_toolbox/lightning_modules/BCE_multilabel.py
+++ dl_toolbox/lightning_modules/BCE_multilabel.py
@@ -12,14 +12,20 @@ import torch.nn.functional as F
 
 from dl_toolbox.lightning_modules.utils import *
 from dl_toolbox.lightning_modules import BaseModule
-from dl_toolbox.networks import UNet
+from dl_toolbox.utils import TorchOneHot
+from networks import *
 
+networks = {
+    'unet': UNet,
+    'smp_unet': SmpUNet
+}
 
-class Unet_CE(BaseModule):
+class BCE_multilabel(BaseModule):
 
-    # CE = Cross Entropy
+    # BCE_multilabel = Binary Cross Entropy for multilabel prediction
 
     def __init__(self,
+                 network,
                  encoder,
                  in_channels,
                  pretrained=True,
@@ -31,23 +37,23 @@ class Unet_CE(BaseModule):
 
         super().__init__(*args, **kwargs)
         
-        self.network = UNet(
-            n_channels=in_channels,
-            n_classes=self.num_classes,
-            bilinear=True
+        self.network = smp.Unet(
+            encoder_name=encoder,
+            encoder_weights='imagenet' if pretrained else None,
+            in_channels=in_channels,
+            classes=self.num_classes,
+            decoder_use_batchnorm=True
         )
         self.in_channels = in_channels
         self.initial_lr = initial_lr
         self.final_lr = final_lr
         self.lr_milestones = list(lr_milestones)
-        self.loss1 = nn.CrossEntropyLoss(
-            ignore_index=self.ignore_index
-        )
-        self.loss2 = DiceLoss(
-            mode="multiclass",
+        self.bce = nn.BCEWithLogitsLoss(reduction='none')
+        self.onehot = TorchOneHot(range(self.num_classes))
+        self.dice = DiceLoss(
+            mode="multilabel",
             log_loss=False,
-            from_logits=True,
-            ignore_index=self.ignore_index
+            from_logits=True
         )
         self.save_hyperparameters()
 
@@ -72,12 +78,24 @@ class Unet_CE(BaseModule):
 
         inputs = batch['image']
         labels = batch['mask']
+        onehot_labels = self.onehot(labels).float()
+        
+        mask = torch.ones_like(
+            onehot_labels,
+            dtype=onehot_labels.dtype,
+            device=onehot_labels.device
+        )
+        if self.ignore_index >= 0:
+            mask -= onehot_labels[:, [self.ignore_index], ...]
+        
         logits = self.network(inputs)
-        loss1 = self.loss1(logits, labels)
-        loss2 = self.loss2(logits, labels)
-        loss = loss1 + loss2
-        self.log('Train_sup_CE', loss1)
-        self.log('Train_sup_Dice', loss2)
+        bce = self.bce(logits, onehot_labels)
+        bce = torch.sum(mask * bce) / torch.sum(mask)
+        dice = self.dice(logits * mask, onehot_labels * mask)
+        loss = bce + dice
+        
+        self.log('Train_sup_BCE', bce)
+        self.log('Train_sup_Dice', dice)
         self.log('Train_sup_loss', loss)
 
         return {'batch': batch, 'logits': logits.detach(), "loss": loss}
@@ -86,8 +104,19 @@ class Unet_CE(BaseModule):
 
         inputs = batch['image']
         labels = batch['mask']
+        onehot_labels = self.onehot(labels).float()
+        
+        mask = torch.ones_like(
+            onehot_labels,
+            dtype=onehot_labels.dtype,
+            device=onehot_labels.device
+        )
+        if self.ignore_index >= 0:
+            mask -= onehot_labels[:, [self.ignore_index], ...]
+        
         logits = self.forward(inputs)
-        preds = logits.argmax(dim=1)
+        probas = torch.sigmoid(logits)
+        preds = torch.argmax(probas, dim=1)
 
         stat_scores = torchmetrics.stat_scores(
             preds,
@@ -95,22 +124,19 @@ class Unet_CE(BaseModule):
             ignore_index=self.ignore_index if self.ignore_index >= 0 else None,
             mdmc_reduce='global',
             reduce='macro',
+            #threshold=0.5,
+            #top_k=1,
             num_classes=self.num_classes
         )
         
-        loss1 = self.loss1(logits, labels)
-        loss2 = self.loss2(logits, labels)
-        loss = loss1 + loss2
-        self.log('Val_CE', loss1)
-        self.log('Val_Dice', loss2)
+        bce = self.bce(logits, onehot_labels)
+        bce = torch.sum(mask * bce) / torch.sum(mask)
+        dice = self.dice(logits * mask, onehot_labels * mask)
+        loss = bce + dice
+        
+        self.log('Val_BCE', bce)
+        self.log('Val_Dice', dice)
         self.log('Val_loss', loss)
-
-        probas = logits.softmax(dim=1)
-        calib_error = torchmetrics.calibration_error(
-            probas,
-            labels
-        )
-        self.log('Calibration error', calib_error)
 
         return {'batch': batch,
                 'logits': logits.detach(),
